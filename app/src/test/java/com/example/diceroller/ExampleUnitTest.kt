8eff7341051690b86f535ed9b46470ad0684fa86
rishi <rishi77177@gmail.com>
2022-05-25 23:21:10 +0530

    Dice Roll

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,14 @@
 package com.example.diceroller
 
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        assertTrue("The dice must rolled between 1 and 6", diceRoll in 1..6)
     }
 }
